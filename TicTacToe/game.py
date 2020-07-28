5d9bb2a54b60319b04c109bea93b11a6b4875773
Nadeem <67517413+nadbad@users.noreply.github.com>
2020-07-28 11:12:07 +0200

    Add files via upload

---
+++ TicTacToe/game.py
@@ -0,0 +1,13 @@
+from board import printBoard
+from game_functions import Game_Function
+
+
+def game():
+
+    Game_Function.game_mechanism(self=1)
+
+    game()
+
+
+if __name__ == "__main__":
+    game()
