3ec41838fe654458ac60d9a9d5d91bc30b5c1d33
Nadeem <67517413+nadbad@users.noreply.github.com>
2020-07-28 11:11:41 +0200

    Delete board.py

--- board.py
+++
@@ -1,6 +0,0 @@
-def printBoard(board):
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
-    print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
